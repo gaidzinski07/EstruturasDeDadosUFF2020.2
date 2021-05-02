ffec458f27fcf3c36786694b8c355ed009f9bdae
Joao Gaidzinski <gaidzinski2110@gmail.com>
2021-05-02 14:59:51 -0300

    Questao 3 VR

--- VR/Q3/Q3.c
+++ VR/Q3/Q3.c
@@ -1,6 +1,41 @@
 #include "TH.h"
 
-//eIgual(char *arq_entrada1, char *arq_entrada2);
+int TAM = 2;
+
+void criaHashing(char *arq, char* hashing, char* dadosHashing){
+  FILE *fp = fopen(dadosHashing, "wb");
+  fclose(fp);
+  TH_inicializa(hashing, TAM);
+  fp = fopen(arq, "rb");
+  fseek(fp, 0, SEEK_SET);
+  int current = 0;
+  while(!feof(fp)){
+    fread(&current, sizeof(int), 1, fp);
+    TH_insere(hashing, dadosHashing, TAM, current);
+  }
+  fclose(fp);
+}
+
+int ehSubconjunto(char *hashing, char *dadosHashing, char *cmpFile){
+  FILE *fp = fopen(cmpFile, "rb");
+  fseek(fp, 0, SEEK_SET);
+  int current = 0;
+  while(!feof(fp)){
+    fread(&current, sizeof(int), 1, fp);
+    if(!TH_busca(hashing, dadosHashing, TAM, current)){
+      fclose(fp);
+      return 0;
+    }
+  }
+  fclose(fp);
+  return 1;
+}
+
+int eIgual(char *arq_entrada1, char *arq_entrada2){
+  criaHashing(arq_entrada1, "hashing_1", "dados_hashing_1");
+  criaHashing(arq_entrada2, "hashing_2", "dados_hashing_2");
+  return ehSubconjunto("hashing_1", "dados_hashing_1", arq_entrada2) && ehSubconjunto("hashing_2", "dados_hashing_2", arq_entrada1);
+}
   
 int main(void){
   int num;
@@ -30,6 +65,6 @@ int main(void){
   }while(1);
   fclose(fp);
   
-  //printf("%d\n", eIgual(char *arq_entrada1, char *arq_entrada2));
+  printf("%d\n", eIgual(nome_dados1, nome_dados2));
   return 0;
 }
