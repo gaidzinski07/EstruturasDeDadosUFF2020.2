5c31438f199cfa938c1adcefb3dbcc0d595272d5
Joao Gaidzinski <gaidzinski2110@gmail.com>
2021-05-03 11:51:04 -0300

    Q1 da VR

--- VR/Q1/Q1.c
+++ VR/Q1/Q1.c
@@ -1,7 +1,59 @@
 #include "TAV.h"
 #include "TAB.h"
+#include <limits.h>
 
-//TAB* av2ab(TAV *a, int *n){}
+int contarFilhos(TAV* a){
+  if(!a){
+    return 0;
+  }
+  TAV *b = a->prim_filho;
+  int cont = 0;
+  do{
+    if(!b)
+      break;
+    else{
+      ++cont;
+      b = b->prox_irmao;
+    }
+  }while(b);
+  return cont;
+}
+
+int max(int a, int b){
+  if(a > b)
+    return a;
+  return b;
+}
+
+int maxN(TAV* a, int maxValue){
+  if(!a){
+    return 0;
+  }
+  TAV* x;
+  for(x = a->prim_filho; x; x = x->prox_irmao){
+    maxValue = max(maxN(x, maxValue), contarFilhos(a));
+  }
+  return maxValue;
+}
+
+TAB *copiaArvore(TAV *a){
+  if(!a){
+    return NULL;
+  }
+  TAV* x = a->prim_filho;
+  TAV* y = x? x->prox_irmao : NULL;
+  return TAB_cria(a->info, copiaArvore(x), copiaArvore(y));
+}
+
+TAB* av2ab(TAV *a, int *n){
+  if(!a){
+    return NULL;
+  }
+  *n = maxN(a, INT_MIN);
+  if(*n > 2)
+    return NULL;
+  return copiaArvore(a);
+}
 
 int main(void){
   int no, pai;
@@ -19,7 +71,7 @@ int main(void){
   }while(1);
   TAV_imprime(a);
   printf("\n");
-  /*
+
   int n;
   TAB *resp = av2ab(a, &n);
   printf("%d\n", n);
@@ -27,7 +79,6 @@ int main(void){
     TAB_imprime(resp);
     TAB_libera(resp);
   }
-  */
   TAV_libera(a);
   return 0;
 }
